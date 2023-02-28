45805085e578f86fb2f4088437a9ecd47eeecc96
PaolaVilla79 <126305424+PaolaVilla79@users.noreply.github.com>
2023-02-28 11:31:40 -0300

    prueba

---
+++ js.js
@@ -0,0 +1,7 @@
+
+     function infoLinkedin(url){
+        window.open(url,"titulo","width=300,height=300");
+     }
+     function abrirEnOtraPest(url){
+        window.open(url,"_blank");
+     }
